8aca204581f90bd4b61034706f8932ae57c1c2ed
Rails Udemy Course <railsudemycourse@gmail.com>
2015-10-29 04:53:27 +0000

    till shoulda matchers

---
+++ app/models/video.rb
@@ -0,0 +1,6 @@
+class Video < ActiveRecord::Base
+  belongs_to :category
+  validates :category_id, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Video.create(title: "Monk", description: "A story about a detective who solves crime",
+  small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg")
+Video.create(title: "Futurama", description: "A story about the future and how people and aliens live in harmony",
+  small_cover_url: "/tmp/futurama.jpg", large_cover_url: "/tmp/futurama_large.jpg")
+Video.create(title: "Family guy", description: "A story about an idiot who does funny things and his family",
+  small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/family_guy_large.jpg")
